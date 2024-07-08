3bbdd6de6c4e3838d6536329ea0313e8fd79adc7
Jin Park <jinpark1504@gmail.com>
2024-07-08 11:49:57 +0900

    webkit

---
+++ WebKitExample/WebKitExample/WebKitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebKitExampleApp.swift
+//  WebKitExample
+//
+//  Created by Jungjin Park on 2024-07-08.
+//
+
+import SwiftUI
+
+@main
+struct WebKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
