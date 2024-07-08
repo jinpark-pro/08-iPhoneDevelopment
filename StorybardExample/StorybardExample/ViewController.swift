f74f0a0fe77b50393db1b9de7c503bf5d4a531c2
Jin Park <jinpark1504@gmail.com>
2024-07-08 13:14:36 +0900

    Add hosting controller and set segue

---
+++ StorybardExample/StorybardExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StorybardExample
+//
+//  Created by Jungjin Park on 2024-07-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
