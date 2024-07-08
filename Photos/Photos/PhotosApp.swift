4bab246b2738a01ca466d3afda64fd35a337a8f2
Jin Park <jinpark1504@gmail.com>
2024-07-08 15:42:27 +0900

    use PhotosUI

---
+++ Photos/Photos/PhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosApp.swift
+//  Photos
+//
+//  Created by Jungjin Park on 2024-07-08.
+//
+
+import SwiftUI
+
+@main
+struct PhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
