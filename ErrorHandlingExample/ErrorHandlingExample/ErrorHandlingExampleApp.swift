911c290217040ee91b648e51a67357f0d54818e8
Jin Park <jinpark1504@gmail.com>
2024-07-09 15:39:08 +0900

    Error handling by using do catch;

---
+++ ErrorHandlingExample/ErrorHandlingExample/ErrorHandlingExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorHandlingExampleApp.swift
+//  ErrorHandlingExample
+//
+//  Created by Jungjin Park on 2024-07-09.
+//
+
+import SwiftUI
+
+@main
+struct ErrorHandlingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
