ed60da7c1130ad806a7cafc5ba46109495d1ce53
Jin Park <jinpark1504@gmail.com>
2024-07-09 13:23:02 +0900

    create CoreMLImage project

---
+++ CoreMLImage/CoreMLImage/CoreMLImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMLImageApp.swift
+//  CoreMLImage
+//
+//  Created by Jungjin Park on 2024-07-09.
+//
+
+import SwiftUI
+
+@main
+struct CoreMLImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
