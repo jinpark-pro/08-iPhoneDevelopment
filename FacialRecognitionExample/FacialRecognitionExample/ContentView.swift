d5506e13d2ccfdd92d8bb8834e5b7c3799447d17
Jin Park <jinpark1504@gmail.com>
2024-07-09 10:47:54 +0900

    display recognized faces

--- FacialRecognitionExample/FacialRecognitionExample/ContentView.swift
+++ FacialRecognitionExample/FacialRecognitionExample/ContentView.swift
@@ -13,13 +13,21 @@ struct ContentView: View {
     
     @State var message = ""
     @State var arrayIndex = 0
+    @State var analyzedImage: UIImage?
     
     var body: some View {
         VStack {
-            Image(photoArray[arrayIndex])
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 250, height: 250)
+            if let image = analyzedImage {
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 500)
+            } else {
+                Image(photoArray[arrayIndex])
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 500)
+            }
             Text(message)
                 .padding()
             Button {
@@ -37,6 +45,7 @@ struct ContentView: View {
                         arrayIndex -= 1
                     }
                     message = ""
+                    analyzedImage = nil
                 } label: {
                     Image(systemName: "chevron.left.square.fill")
                         .font(.largeTitle)
@@ -48,6 +57,7 @@ struct ContentView: View {
                         arrayIndex += 1
                     }
                     message = ""
+                    analyzedImage = nil
                 } label: {
                     Image(systemName: "chevron.right.square.fill")
                         .font(.largeTitle)
@@ -58,7 +68,7 @@ struct ContentView: View {
     }
     func analyzeImage(image: UIImage) {
         let handler = VNImageRequestHandler(cgImage: image.cgImage!, options: [:])
-        let request = VNDetectFaceRectanglesRequest(completionHandler: handleFaceRecognition)
+        let request = VNDetectFaceLandmarksRequest(completionHandler: handleFaceRecognition)
         try! handler.perform([request])
     }
     
@@ -67,6 +77,39 @@ struct ContentView: View {
             fatalError("Can't find a face in the picture")
         }
         message = "Found \(foundFaces.count) faces in the picture"
+        
+        guard let image = UIImage(named: photoArray[arrayIndex]) else { return }
+        
+        // 이미지 생성을 위한 컨텍스트 시작
+        UIGraphicsBeginImageContextWithOptions(image.size, false, 0.0)
+        
+        // 컨텍스트 변수 가져오기
+        let context = UIGraphicsGetCurrentContext()!
+        
+        // 컨텍스트 내에 이미지 그리기
+        image.draw(in: CGRect(origin: .zero, size: image.size))
+        
+        // 이미지를 그린 후 좌표계를 변환
+        context.translateBy(x: 0, y: image.size.height)
+        context.scaleBy(x: 1.0, y: -1.0)
+        
+        // 얼굴 주변 사각형을 그리기 위한 선 색상과 두께 설정
+        context.setStrokeColor(UIColor.red.cgColor)
+        context.setLineWidth(5)
+        
+        for faceObservation in foundFaces {
+            // 정규화된 얼굴 인식 위치값 가져오기
+            let faceRect = VNImageRectForNormalizedRect(faceObservation.boundingBox, Int(image.size.width), Int(image.size.height))
+            // 가져온 위치에 테두리 그리기
+            context.stroke(faceRect)
+        }
+        
+        // 컨텍스트를 종료하고, 컨텍스트 결과를 이미지 변수에 할당
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        // 위의 컨텍스트 명령 실행
+        UIGraphicsEndImageContext()
+        // 만들어진 이미지를 화면에 출력
+        analyzedImage = newImage
     }
 }
 
