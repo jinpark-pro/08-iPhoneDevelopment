2a6eb1810986cc082fde7353beb7e455eb419c03
Jin Park <jinpark1504@gmail.com>
2024-07-09 10:08:07 +0900

    face recognition

---
+++ FacialRecognitionExample/FacialRecognitionExample/FacialRecognitionExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacialRecognitionExampleApp.swift
+//  FacialRecognitionExample
+//
+//  Created by Jungjin Park on 2024-07-09.
+//
+
+import SwiftUI
+
+@main
+struct FacialRecognitionExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
