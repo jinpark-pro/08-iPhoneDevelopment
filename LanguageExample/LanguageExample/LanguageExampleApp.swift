86178c5a5525c5a5dbd019968c871d9e803d691d
Jin Park <jinpark1504@gmail.com>
2024-07-09 14:36:05 +0900

    language recognizer

---
+++ LanguageExample/LanguageExample/LanguageExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LanguageExampleApp.swift
+//  LanguageExample
+//
+//  Created by Jungjin Park on 2024-07-09.
+//
+
+import SwiftUI
+
+@main
+struct LanguageExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
